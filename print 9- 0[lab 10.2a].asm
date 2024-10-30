94524c7470976c589d88265c3a2fee8b67ea5a7f
balaji-pulivarthi <159748740+balaji-pulivarthi@users.noreply.github.com>
2024-10-30 12:05:33 +0530

    Add files via upload
    Write a program in assembly language to print the numbers from 0 to 9 in reverse order.

---
+++ print 9- 0[lab 10.2a].asm
@@ -0,0 +1,33 @@
+org 100h               ; Origin for COM file format
+
+mov dx,offset output 
+mov ah,09h
+int 21h
+
+; Print numbers from 9 to 0
+mov cx, 10             ; Loop counter set to 10 (for numbers 0 to 9)
+mov al, 9              ; Start with the number 0
+
+print:
+    ; Convert number in AL to ASCII character
+    add al, '0'        ; Convert number to ASCII
+
+    ; Print the character
+    mov dl, al         ; Move the ASCII character to DL
+    mov ah, 02h        ; DOS function 02h: print character
+    int 21h            ; Call DOS interrupt
+    
+    
+    
+    ; Prepare for the next iteration
+    sub al, '0'        ; Convert back to integer
+    dec al             ; decrement the number
+    loop print         ; Loop until CX is zero
+
+; Terminate the program
+mov ah, 4Ch            ; DOS function 4Ch: terminate program
+int 21h                ; Call DOS interrupt to exit
+
+output db "The numbers from 9-0: $ " 
+
+end                     ; End of program
