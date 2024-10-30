8acbe930276d74053683602d12a2c07cfb17b20d
balaji-pulivarthi <159748740+balaji-pulivarthi@users.noreply.github.com>
2024-10-30 12:05:02 +0530

    Add files via upload
    Write an assembly language program to print the characters from A to Z in reverse order.

---
+++ print  Z - A [lab 10.1b].asm
@@ -0,0 +1,29 @@
+org 100h               ; Origin for COM file format
+
+jmp start:
+output db "The alphabets from Z-A: $ "
+
+start:
+mov dx,offset output 
+mov ah,09h
+int 21h
+
+; Print characters from 'Z' to 'A'
+mov cx, 26             ; Loop counter set to 26 (for letters A to Z)
+mov al, 'Z'            ; Start with the character 'Z'
+
+print:
+    ; Print the character
+    mov dl, al         ; Move the character to DL
+    mov ah, 02h        ; DOS function 02h: print character
+    int 21h            ; Call DOS interrupt
+
+    ; Prepare for the next iteration
+    dec al             ; Move to the previous character
+    loop print         ; Loop until CX is zero
+
+; Terminate the program
+mov ah, 4Ch            ; DOS function 4Ch: terminate program
+int 21h                ; Call DOS interrupt to exit
+
+end                     ; End of program
